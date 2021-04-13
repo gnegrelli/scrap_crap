df870e67db5753fc30aa871377c7d739d1577485
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2021-04-12 22:05:57 -0300

    Use regex to find phones and save data in dict

--- scrap/scrap/spiders/spider.py
+++ scrap/scrap/spiders/spider.py
@@ -1,11 +1,13 @@
 import re
 import scrapy
 
+from bs4 import BeautifulSoup
 from pathlib import Path
 
 
 class MySpider(scrapy.Spider):
 	name = 'my_spider'
+	re_phone = re.compile(r'(\+\s*\d{1,3})?\s*(\(\s*\d{1,4}\s*\))?\s*([\d\- ]{6,})\b')
 
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
@@ -13,9 +15,19 @@ class MySpider(scrapy.Spider):
 		if filename.is_file():
 			with open(filename, 'r') as f:
 				self.start_urls = [url.strip() for url in f.read().split('\n') if url.strip()]
+		self.start_urls = self.start_urls[:2]
 
 	def parse(self, response):
-		for img in response.css('img'):
-			if any(re.search('logo', img.attrib.get(attr, ''), re.IGNORECASE) for attr in ('class', 'id', 'src', 'tag')):
-				print(img.attrib['src'])
-		print(50*'~')
+		d = {'logos': [], 'phones': [], 'website': str(response.url)}
+		soup = BeautifulSoup(response.body, features='lxml')
+		text = re.sub(r'\s{2,}', ' ', soup.get_text().strip())
+		phones = self.re_phone.findall(text)
+		with open('/home/psiu/Desktop/crap.txt', 'a+') as f:
+			print(text, file=f)
+		d['phones'] = [
+			phone.join(' ').strip() for phone in phones
+		]
+		d['logos'] = [
+			img.attrib['src'] for img in response.css('img') if 
+			any(re.search('logo', img.attrib.get(attr, ''), re.IGNORECASE) for attr in ('class', 'id', 'src', 'tag'))
+		]
